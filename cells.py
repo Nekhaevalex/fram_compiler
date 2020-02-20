1f49473e3e9e705fc84202a92c7293ee5af6b7db
mik <mik@pop-os.localdomain>
2020-02-20 18:19:20 +0300

    process sen_amp

--- cells.py
+++ cells.py
@@ -71,5 +71,31 @@ class Memory_Cell(My_Cell):
 
 class Sense_Amp(My_Cell):
 	"""docstring for ClassName"""
-	def __init__(self,cell):
-		pass		
+	def __init__(self,cells):
+		self.cells = cells
+		self.boundary_box = ()
+
+	def find_boundary(self,layer= None): #default layer is PR Bndry
+		k = 0
+		for cell in cells:
+			if (layer == None):
+				boundary[k] = self.cell.bbox()
+			if (layer != None):
+				boundary[k] = self.cell.bbox_per_layer(layer)
+		return boundary
+
+	def find_pin_map(self, layers): ## -------------- FIX!----------
+		'''create dictionary with text klayout.Text objects and its klayout.Point s'''
+		h = 0
+		pin_map = ({},{})
+		for cell in cells:
+			for layer in layers:
+				for i in self.cell[h].each_shape(layer):
+				#print("===== + 1 =======")
+					if (i.is_text()):
+						pin_map[h][i.text_string] = i
+						#pin_map[i.text_string+'_layer'] 
+						#print(i.text) - text object
+						#print(i.text_string)# - text itself
+		self.pin_map = pin_map
+		return pin_map
